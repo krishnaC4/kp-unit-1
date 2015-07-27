b2fbfefb3616217a52534c899d46270ba2d8c6c0
MacMan <MacMan@MacMans-iMac-3.local>
2015-07-27 14:09:45 -0400

    unit-1 week-0

---
+++ week0/kpToDoApp/kpToDoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kpToDoApp
+//
+//  Created by MacMan on 7/26/15.
+//  Copyright (c) 2015 MacManApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ week0/kpToDoApp/kpToDoApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kpToDoApp
+//
+//  Created by MacMan on 7/26/15.
+//  Copyright (c) 2015 MacManApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
